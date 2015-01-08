30799bdd5c3c24569b04fc4a0dd94d1cd49741fc
Andrzej Oleś <andrzej.oles@gmail.com>
2015-01-08 16:35:52 +0000

    Add annotation for convenient downstream processing
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/Hiiragi2013@3158 db2202da-8704-0410-b924-b125b3cd84ef

--- inst/scripts/readdata.R
+++ inst/scripts/readdata.R
@@ -2,6 +2,7 @@ library("affy")
 library("ArrayExpress")
 library("arrayQualityMetrics")
 library("mouse4302.db")
+library("RColorBrewer")
 
 CELdir    = tempdir()
 CELfiles  = getAE("E-MTAB-1681", path = CELdir, type = "raw")$rawFiles
@@ -40,6 +41,7 @@ readCSVtable = function(name) {
     x$lineage = rep(NA, nrow(x))
   }
   
+  row.names(x) = paste(x$File.name, "CEL", sep = ".")
   return(x)
 }
 
@@ -53,7 +55,7 @@ pdata$genotype = as.factor(ifelse(grepl("_KO$", pdata$File.name), "FGF4-KO", "WT
 ## Read the CEL files
 ## -------------------------------------------------------------------------------------
 
-fileNames = paste(pdata$File.name, "CEL", sep = ".")
+fileNames = row.names(pdata)
 fileExists = (fileNames %in% CELfiles)
 stopifnot(all(fileExists))
 
@@ -63,7 +65,7 @@ a = ReadAffy(filenames = fileNames, celfile.path = CELdir, phenoData = pdata,
 pData(a)$ScanDate = factor(as.Date(sub( "10/16/09", "2010-09-16",
     sapply(strsplit( protocolData(a)$ScanDate, split = "[T ]" ), `[`, 1) )))
 
-save(a, file="a.rda")
+save(a, file="a.rda", compress="xz")
 
 ## -------------------------------------------------------------------------------------
 ## Normalize with RMA
@@ -84,4 +86,50 @@ fData(x)$symbol   = annotateGene(mouse4302SYMBOL,   "symbol", missing = featureN
 fData(x)$genename = annotateGene(mouse4302GENENAME, "gene_name", missing = "")
 fData(x)$ensembl  = annotateGene(mouse4302ENSEMBL,  "ensembl_id", missing = "")
 
-save(x, file="x.rda")
+## -------------------------------------------------------------------------------------
+## Grouping of samples 
+## -------------------------------------------------------------------------------------
+
+## We define a grouping of the samples and an associated colour map, which will
+## be used in the plots throughout this report.
+
+groups = with(pData(x), list(
+  `E3.25`           = which(genotype=="WT" & Embryonic.day=="E3.25"),
+  `E3.5 (EPI)`      = which(genotype=="WT" & Embryonic.day=="E3.5" & lineage=="EPI"),
+  `E4.5 (EPI)`      = which(genotype=="WT" & Embryonic.day=="E4.5" & lineage=="EPI"),
+  `E3.5 (PE)`       = which(genotype=="WT" & Embryonic.day=="E3.5" & lineage=="PE"),
+  `E4.5 (PE)`       = which(genotype=="WT" & Embryonic.day=="E4.5" & lineage=="PE"),
+  `E3.25 (FGF4-KO)` = which(genotype=="FGF4-KO" & Embryonic.day=="E3.25"),
+  `E3.5 (FGF4-KO)`  = which(genotype=="FGF4-KO" & Embryonic.day=="E3.5"),
+  `E4.5 (FGF4-KO)`  = which(genotype=="FGF4-KO" & Embryonic.day=="E4.5")))
+
+sampleColourMap = character(length(groups))
+names(sampleColourMap) = names(groups)
+sampleColourMap[c("E3.5 (EPI)", "E4.5 (EPI)")]         = brewer.pal(10, "Paired")[1:2]
+sampleColourMap[c("E3.5 (PE)", "E4.5 (PE)")]           = brewer.pal(10, "Paired")[3:4]
+sampleColourMap[c("E3.25 (FGF4-KO)")]                  = brewer.pal(10, "Paired")[c(7)]
+sampleColourMap[c("E3.5 (FGF4-KO)", "E4.5 (FGF4-KO)")] = brewer.pal(10, "Paired")[c(8,6)]
+sampleColourMap[c("E3.25")]                            = brewer.pal(12, "Paired")[c(9)]
+stopifnot(!any(sampleColourMap==""))
+
+## The following assertions aim to make sure that each sample was assigned to
+## exactly one group.
+stopifnot(!any(duplicated(unlist(groups))),
+            setequal(unlist(groups), seq_len(ncol(x))),
+            setequal(names(sampleColourMap), names(groups)))
+
+## Next, assign a colour and a name to each sample, which will be used in the
+## subsequent plots. For sample names, use the group name and the array numeric
+## index.
+
+sampleNames = sampleGroup = rep(NA_character_, ncol(x))
+for(i in seq(along = groups)) {
+  idx = groups[[i]]
+  sampleGroup[idx] = names(groups)[i]
+  sampleNames[idx] = paste(idx, names(groups)[i])
+}
+pData(x)$sampleGroup  = sampleGroup
+pData(x)$sampleColour = sampleColourMap[sampleGroup]
+sampleNames(x) = sampleNames
+
+save(x, file="x.rda", compress="xz")
